9053d176a53e6dae26aab465d1b72776b59de8a5
Demon Singur <demonsingur@gmail.com>
2018-07-14 14:37:51 +0000

    sdm660-common: do not build useless packages
    They are compiled as dependencies of ipacm
    
    Change-Id: I72076113083308e26573289923f5d3c00a8a6b44

--- device.mk
+++ device.mk
@@ -563,7 +563,6 @@ PRODUCT_PACKAGES += \
     android.hardware.radio.config@1.1 \
     android.hardware.secure_element@1.0 \
     librmnetctl \
-    libxml2 \
     libprotobuf-cpp-full \
     qti-telephony-hidl-wrapper \
     qti_telephony_hidl_wrapper.xml \
@@ -613,10 +612,7 @@ PRODUCT_PACKAGES += \
 # Tetheroffload
 PRODUCT_PACKAGES += \
     ipacm \
-    ipacm.rc \
-    IPACM_cfg.xml \
-    libipanat \
-    liboffloadhal
+    IPACM_cfg.xml
 
 # TextClassifier smart selection model files
 PRODUCT_PACKAGES += \
