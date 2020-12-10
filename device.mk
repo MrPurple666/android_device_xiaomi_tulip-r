1b5cf63c5cc5bc7316f6901be63ef2101d43b964
sbwml <984419930@qq.com>
2020-12-10 13:49:03 +0800

    tulip: bump vendor.display.config to 2.0
    * New WFD blobs need vendor.display.confi@2.0.so

--- device.mk
+++ device.mk
@@ -216,8 +216,8 @@ PRODUCT_PACKAGES += \
     libqdMetaData \
     libqdMetaData.system \
     libtinyxml \
-    vendor.display.config@1.9 \
-    vendor.display.config@1.9_vendor
+    vendor.display.config@2.0 \
+    vendor.display.config@2.0.vendor
 
 # DRM
 PRODUCT_PACKAGES += \
