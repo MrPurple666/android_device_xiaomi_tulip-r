97a01b966b89706570b31a8023052f6adff424e3
jefinhodatnt <jefiinhorelikia@gmail.com>
2021-01-15 02:21:13 -0300

    Update Fp jan 2021 -->Redfin
    Signed-off-by: jefinhodatnt <jefiinhorelikia@gmail.com>

--- aosp_tulip.mk
+++ aosp_tulip.mk
@@ -40,8 +40,11 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 TARGET_VENDOR_PRODUCT_NAME := tulip
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="tulip-user 9 PKQ1.180904.001 V11.0.6.0.PEKMIXM release-keys"
+    PRIVATE_BUILD_DESC="redfin-user 11 RQ1A.210105.003 7005429 release-keys"
 
 #Build FP to be picked by both system and vendor
-BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ1A.201205.010/6953398:user/release-keys"
+BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ1A.210105.003/7005429:user/release-keys"
+
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.build.fingerprint=$(BUILD_FINGERPRINT)
 
