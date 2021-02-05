87aabff04ade5f48b793de4fd5bbca85049feec2
jefinhodatnt <jefiinhorelikia@gmail.com>
2021-02-05 17:42:19 -0300

    Update FP to feb
    Signed-off-by: jefinhodatnt <jefiinhorelikia@gmail.com>

--- aosp_tulip.mk
+++ aosp_tulip.mk
@@ -40,8 +40,8 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 TARGET_VENDOR_PRODUCT_NAME := tulip
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="tulip-user 9 PKQ1.180904.001 V12.0.1.0.PEKMIXM release-keys"
+    PRIVATE_BUILD_DESC="redfin-user 11 RQ1A.210205.004 7038034 release-keys"
 
-BUILD_FINGERPRINT := "google/coral/coral:11/RQ1A.210105.003/7005429:user/release-keys"
+BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ1A.210205.004/7038034:user/release-keys"
 
 
