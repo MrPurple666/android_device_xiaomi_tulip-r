3b5f2e9a04ad90001da04dbabb662cf86b367126
Mr_Purple_666 <78635632+MrPurple666@users.noreply.github.com>
2021-02-10 02:13:37 +0000

    Deleted unnecessary vendorsetup
    After removing the full immersive gestures this vendorsetup is unnecessary

--- vendorsetup.sh
+++
@@ -1,4 +0,0 @@
-rm -rf frameworks/base/package/overlays/NavigationBarModeGesturalOverlay
-rm -rf frameworks/base/package/overlays/NavigationBarModeGesturalOverlayWideBack
-rm -rf frameworks/base/package/overlays/NavigationBarModeGesturalOverlayNarrowBack
-rm -rf frameworks/base/package/overlays/NavigationBarModeGesturalOverlayExtraWideBack
