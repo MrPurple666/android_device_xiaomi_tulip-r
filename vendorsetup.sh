77d58b14bd3233d75ca27207f97871ce4593a9da
jefinhodatnt <jefiinhorelikia@gmail.com>
2021-02-12 19:22:37 -0300

    Tulip: Fix path imqs
    Signed-off-by: jefinhodatnt <jefiinhorelikia@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,4 @@
-rm -rf frameworks/base/package/overlays/NavigationBarModeGesturalOverlay
-rm -rf frameworks/base/package/overlays/NavigationBarModeGesturalOverlayWideBack
-rm -rf frameworks/base/package/overlays/NavigationBarModeGesturalOverlayNarrowBack
-rm -rf frameworks/base/package/overlays/NavigationBarModeGesturalOverlayExtraWideBack
+rm -rf frameworks/base/packages/overlays/NavigationBarModeGesturalOverlay
+rm -rf frameworks/base/packages/overlays/NavigationBarModeGesturalOverlayWideBack
+rm -rf frameworks/base/packages/overlays/NavigationBarModeGesturalOverlayNarrowBack
+rm -rf frameworks/base/packages/overlays/NavigationBarModeGesturalOverlayExtraWideBack
