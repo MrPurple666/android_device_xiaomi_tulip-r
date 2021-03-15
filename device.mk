e3a8c3fcb344b4daae4ea602c6ef8bbdf6d79f15
OdSazib <odsazib@gmail.com>
2021-03-15 12:21:09 +0600

    Tulip: Build power stats and label it

--- device.mk
+++ device.mk
@@ -462,7 +462,8 @@ PRODUCT_PACKAGES += \
 #    android.hardware.power-service-qti
 
 PRODUCT_PACKAGES += \
-    android.hardware.power-service.xiaomi_sdm660-libperfmgr
+    android.hardware.power-service.xiaomi_sdm660-libperfmgr \
+    android.hardware.power.stats@1.0-service.mock
 
 # Powerhint
 ifeq ($(EAS_POWERHINT_VARIANT), sdm636)
