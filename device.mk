7ef1e019172fbd23318acb904394a8089c0deabe
Manish4586 <Manish.n.manish45@gmail.com>
2021-03-24 00:27:43 -0300

    Tulip: audio: Switch to audio hal v4
    Signed-off-by: jefinhodatnt <jefiinhorelikia@gmail.com>

--- device.mk
+++ device.mk
@@ -54,9 +54,9 @@ TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 
 # Audio
 PRODUCT_PACKAGES += \
-    android.hardware.audio@6.0-impl \
+    android.hardware.audio@4.0-impl \
     android.hardware.audio@2.0-service \
-    android.hardware.audio.effect@6.0-impl \
+    android.hardware.audio.effect@4.0-impl \
     android.hardware.audio.effect@2.0-service \
     android.hardware.soundtrigger@2.2-impl\
     audio.a2dp.default \
