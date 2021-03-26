b2ce56d13772411fd8060ccb6baefce4ee76bd21
jefinhodatnt <jefiinhorelikia@gmail.com>
2021-03-26 20:26:40 -0300

    Tulip: Drop non-functional soundtrigger * Fixes google assistant voice match in android 11
    Signed-off-by: jefinhodatnt <jefiinhorelikia@gmail.com>

--- device.mk
+++ device.mk
@@ -58,7 +58,6 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-service \
     android.hardware.audio.effect@4.0-impl \
     android.hardware.audio.effect@2.0-service \
-    android.hardware.soundtrigger@2.2-impl\
     audio.a2dp.default \
     audio_amplifier.sdm660 \
     audio.primary.sdm660 \
