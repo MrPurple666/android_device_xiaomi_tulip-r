44d8855b7712a9f292c9f36e3375aac16e2c51ad
MrPurple666 <antoniosacramento666usa@gmail.com>
2021-10-08 19:33:57 -0300

    back neverallows

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -261,7 +261,7 @@ else
         include device/qcom/sepolicy-legacy-um/sepolicy.mk
 endif
 
-#SELINUX_IGNORE_NEVERALLOWS := true
+SELINUX_IGNORE_NEVERALLOWS := true
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/public
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
